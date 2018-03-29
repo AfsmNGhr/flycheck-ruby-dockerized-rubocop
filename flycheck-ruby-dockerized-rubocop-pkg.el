f90d8d583a632e413005b08f76c152a2d2e9be68
Afay Zangetsu <afay.zangetsu@gmail.com>
2018-03-29 10:52:49 +0300

    fix: del pkg ...

--- flycheck-ruby-dockerized-rubocop-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "flycheck-ruby-dockerized-rubocop"
-  "20180218.1857"
-  "Dockerized rubocop flycheck checker"
-  '((emacs "24.4")
-    (flycheck "31"))
-  :url "https://github.com/AfsmNGhr/flycheck-ruby-dockerized-rubocop")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
